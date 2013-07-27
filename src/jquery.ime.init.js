a3dd350e00f3bae26280e42f206fb53b43c89e50
Praveen <prageck@gmail.com>
2013-07-28 01:42:15 +0530

    using extension's preference rather than site specific local storage

--- src/jquery.ime.init.js
+++ src/jquery.ime.init.js
@@ -3,16 +3,19 @@ $( document ).ready( function () {
 	$.extend( $.ime.preferences, {
 
 		save: function () {
-			localStorage.setItem( 'imepreferences', JSON.stringify( this.registry ) );
+			chrome.storage.local.set( { 'imepreferences': JSON.stringify( this.registry ) } );
 		},
 
 		load: function () {
-			this.registry = JSON.parse( localStorage.getItem( 'imepreferences' ) ) || this.registry;
+			var imePreferences = this;
+			chrome.storage.local.get( ['imepreferences'], function ( result ) {
+				imePreferences.registry = JSON.parse( result.imepreferences ) || imePreferences.registry;
+				initializeIME();
+			} );
 		}
 
 	} );
 
-	
 	function quickList() {
 		var unique = [ $( 'html' ).attr( 'lang' ) || 'en' ],
 			previousIMELanguages;
@@ -40,75 +43,77 @@ $( document ).ready( function () {
 	// Load the ime preferences
 	$.ime.preferences.load();
 	
-	$( 'body' ).on( 'focus.ime', 'input:not([type]), input[type=text], input[type=search], textarea', function () {
-		var $input = $( this );
-		$input.ime( {
-			languages: quickList(),
-			languageSelector: function () {
-				var $ulsTrigger;
-	
-				$ulsTrigger = $( '<a>' ).text( '...' )
-					.addClass( 'ime-selector-more-languages selectable-row selectable-row-item' );
+	function initializeIME() {
+		$( 'body' ).on( 'focus.ime', 'input:not([type]), input[type=text], input[type=search], textarea', function () {
+			var $input = $( this );
+			$input.ime( {
+				languages: quickList(),
+				languageSelector: function () {
+					var $ulsTrigger;
+		
+					$ulsTrigger = $( '<a>' ).text( '...' )
+						.addClass( 'ime-selector-more-languages selectable-row selectable-row-item' );
 
-				$ulsTrigger.uls( {
-					onSelect: function ( language ) {
-						$input.data( 'imeselector' ).selectLanguage( language );
-						$input.focus();
-					},
-					lazyload: false,
-					languages: availableLanguages()
-					// top: $( window ).height()/2 - 214,
-					// left: $( window ).width()/2 - 358
-				} );
-	
-				return $ulsTrigger;
-			},
-			helpHandler: function ( ime ) {
-				return $( '<a>' )
-					.attr( {
-						href: 'https://www.mediawiki.org/wiki/Special:MyLanguage/Help:Extension:UniversalLanguageSelector/Input_methods/$1'.replace( '$1', ime ),
-						target: '_blank',
-						title: 'How to use'
-					} )
-					.addClass( 'ime-perime-help' )
-					.click( function ( event ) {
-						event.stopPropagation();
+					$ulsTrigger.uls( {
+						onSelect: function ( language ) {
+							$input.data( 'imeselector' ).selectLanguage( language );
+							$input.focus();
+						},
+						lazyload: false,
+						languages: availableLanguages()
+						// top: $( window ).height()/2 - 214,
+						// left: $( window ).width()/2 - 358
 					} );
-			}
-		} );
-
-		$input.data( 'ime' ).constructor.prototype.load = function ( inputmethodId ) {
-			var ime = this,
-				deferred = $.Deferred(),
-				dependency,
-				runtimeOrExtension;
-
-			if ( $.ime.inputmethods[inputmethodId] ) {
-				return deferred.resolve();
-			}
-
-			dependency = $.ime.sources[inputmethodId].depends;
-			if ( dependency ) {
-				return $.when( this.load( dependency ), this.load( inputmethodId ) );
-			}
-
-			// Determining which method the Google Chrome is using.
-			// Until Chrome version 25 message passing between content scripts and 
-			// extension scripts was implemented using chrome.extension.sendmessage. 
-			// From version 26 onwards it is implemented using chrome.runtime.sendmessage.
-			runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';
-
-			chrome[runtimeOrExtension].sendMessage( { fileToInject: $.ime.sources[inputmethodId].source }, function ( response ) {
-				if ( response.injected ) {
-					console.log( inputmethodId + ' loaded.' );
-					deferred.resolve();
-				}
-				else {
-					console.log( 'Error in loading inputmethod ' + name + ' Error: ' + response.errorMessage );
+		
+					return $ulsTrigger;
+				},
+				helpHandler: function ( ime ) {
+					return $( '<a>' )
+						.attr( {
+							href: 'https://www.mediawiki.org/wiki/Special:MyLanguage/Help:Extension:UniversalLanguageSelector/Input_methods/$1'.replace( '$1', ime ),
+							target: '_blank',
+							title: 'How to use'
+						} )
+						.addClass( 'ime-perime-help' )
+						.click( function ( event ) {
+							event.stopPropagation();
+						} );
 				}
 			} );
 
-			return deferred.promise();
-		}
-	} );
+			$input.data( 'ime' ).constructor.prototype.load = function ( inputmethodId ) {
+				var ime = this,
+					deferred = $.Deferred(),
+					dependency,
+					runtimeOrExtension;
+
+				if ( $.ime.inputmethods[inputmethodId] ) {
+					return deferred.resolve();
+				}
+
+				dependency = $.ime.sources[inputmethodId].depends;
+				if ( dependency ) {
+					return $.when( this.load( dependency ), this.load( inputmethodId ) );
+				}
+
+				// Determining which method the Google chrome is using.
+				// Until chrome version 25 message passing between content scripts and 
+				// extension scripts was implemented using chrome.extension.sendmessage. 
+				// From version 26 onwards it is implemented using chrome.runtime.sendmessage.
+				runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';
+
+				chrome[runtimeOrExtension].sendMessage( { fileToInject: $.ime.sources[inputmethodId].source }, function ( response ) {
+					if ( response.injected ) {
+						console.log( inputmethodId + ' loaded.' );
+						deferred.resolve();
+					}
+					else {
+						console.log( 'Error in loading inputmethod ' + name + ' Error: ' + response.errorMessage );
+					}
+				} );
+
+				return deferred.promise();
+			}
+		} );
+	}
 } );
